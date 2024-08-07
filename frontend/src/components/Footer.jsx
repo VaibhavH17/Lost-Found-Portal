5e24c7bf76c0ca86b722cb8a3033b677be46daf3
VaibhavH17 <vaibhavhebale17@gmail.com>
2024-08-07 18:52:02 +0530

    Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -15,7 +15,7 @@ function Footer() {
         </section>
       </div>
       <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
-        © 2023 Akash Solutions Pvt. Ltd.
+       
       </div>
     </footer>
   );
